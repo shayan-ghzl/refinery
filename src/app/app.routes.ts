be0372b432d9caed52785943d43b98b55cccd0cc
Shayan Ghazali <shayan97.tti@gmail.com>
2025-10-14 19:35:17 +0330

    styles init

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { Add } from './pages/add/add';
+import { List } from './pages/list/list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'list', component: List },
+    { path: 'add', component: Add },
+    { path: '**', redirectTo: 'list' },
+];
